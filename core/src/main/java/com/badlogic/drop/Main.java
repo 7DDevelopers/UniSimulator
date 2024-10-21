d77adb5a47593306fd1643d79f49a3bf5f158d54
luke <luke.a.timmons@gmail.com>
2024-10-21 14:14:51 +0100

    added start menu

--- core/src/main/java/com/badlogic/drop/Main.java
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -19,6 +20,17 @@ import org.w3c.dom.Text;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 import java.awt.*;
 import java.awt.image.renderable.RenderContext;
@@ -41,9 +53,35 @@ public class Main implements ApplicationListener {
     private SpriteBatch batch;
 
     public int STAGE = 0;
+    private Stage startMenuStage;
+    private Skin skin;
 
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        startMenuStage = new Stage(new ScreenViewport());
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        Gdx.input.setInputProcessor(startMenuStage);
+
+        TextButton startButton = new TextButton("Start", skin);
+
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                STAGE = 1;
+                System.out.println("Game State changed to: " + STAGE);  // For debugging
+            }
+        });
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center();
+
+        table.add(startButton).width(200).height(60);
+
+        startMenuStage.addActor(table);
         viewport = new FitViewport(800, 450);
 
         inputManager = new InputManager((OrthographicCamera)viewport.getCamera());
@@ -66,11 +104,19 @@ public class Main implements ApplicationListener {
 
     @Override
     public void render() {
-        mainGame();
+        if(STAGE == 1){mainGame();}
+        if(STAGE==0){startMenu();}
 
     }//
+    public void startMenu(){
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    public void mainGame{
+        // Draw the stage
+        startMenuStage.act(Gdx.graphics.getDeltaTime());
+        startMenuStage.draw();
+    }
+    public void mainGame(){
         handleInput();
 
         //Update camera
