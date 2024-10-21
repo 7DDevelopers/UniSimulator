747f6ef355632937d062740a241bc4be3a28f65f
luke <luke.a.timmons@gmail.com>
2024-10-21 13:53:09 +0100

    added entity

---
+++ core/src/main/java/com/badlogic/drop/Entity.java
@@ -0,0 +1,35 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Entity {
+    public Texture texture;
+    private Vector2 position;
+
+    public Entity(float x, float y, String textureLocation) {
+        // Load the Badlogic texture
+        texture = new Texture(Gdx.files.internal(textureLocation)); // Make sure you have this file in your assets folder
+        position = new Vector2(x, y);
+    }
+
+    public void render(SpriteBatch spriteBatch) {
+        spriteBatch.begin();
+        spriteBatch.draw(texture, position.x, position.y);
+        spriteBatch.end();
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public void setPosition(float x, float y) {
+        position.set(x, y);
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+}
